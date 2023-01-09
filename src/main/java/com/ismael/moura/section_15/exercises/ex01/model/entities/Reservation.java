5b7e41cb0d38fc78b6627ce59485ea87bd429c4d
Ismael Moura <ismaeldesousa2014@outlook.com>
2023-01-09 11:10:25 -0300

    reservertion exercise: bad solution

--- src/main/java/com/ismael/moura/section_15/exercises/ex01/model/entities/Reservation.java
+++ src/main/java/com/ismael/moura/section_15/exercises/ex01/model/entities/Reservation.java
@@ -1,6 +1,5 @@
 package com.ismael.moura.section_15.exercises.ex01.model.entities;
 
-import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -39,9 +38,18 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkIn, Date checkOut) {
+    public String updateDates(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            return "Reservation dates for update must be future dates";
+        }
+        if (!checkOut.after(checkIn)) {
+            return "Check-out date must be after check-in date";
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+
+        return null;
     }
 
     @Override
