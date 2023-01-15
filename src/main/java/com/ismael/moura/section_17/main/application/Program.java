ad9fd5c3ade0a01c25e01c39c0eb85824d26f28e
Ismael Moura <ismaeldesousa2014@outlook.com>
2023-01-15 13:45:51 -0300

    information of file path

--- src/main/java/com/ismael/moura/section_17/main/application/Program.java
+++ src/main/java/com/ismael/moura/section_17/main/application/Program.java
@@ -1,35 +1,21 @@
 package com.ismael.moura.section_17.main.application;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a folder path: ");
+        System.out.print("Enter a file path: ");
         String strPath = scanner.nextLine();
 
         File path = new File(strPath);
 
-        File[] folders = path.listFiles(File::isDirectory);
-        System.out.println("FOLDERS: ");
-        if (folders != null) {
-            for (File folder : folders) {
-                System.out.println(folder);
-            }
-        }
-
-        File[] files = path.listFiles(File::isFile);
-        System.out.println("FILES: ");
-        if (files != null) {
-            for (File file : files) {
-                System.out.println(file);
-            }
-        }
-
-        boolean success = new File(strPath + "\\subdirTESTE").mkdir();
-        System.out.println("Directory created successfully: " + success);
+        System.out.println("getName: " + path.getName());
+        System.out.println("getParent: " + path.getParent());
+        System.out.println("getPath: " + path.getPath());
 
         scanner.close();
     }
