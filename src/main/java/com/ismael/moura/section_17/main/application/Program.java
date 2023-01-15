3b86780a3645a173543c8c9357013a94f43ce31c
Ismael Moura <ismaeldesousa2014@outlook.com>
2023-01-15 09:48:47 -0300

    216. try-catch with resources

--- src/main/java/com/ismael/moura/section_17/main/application/Program.java
+++ src/main/java/com/ismael/moura/section_17/main/application/Program.java
@@ -7,12 +7,8 @@ import java.io.IOException;
 public class Program {
     public static void main(String[] args) {
         String path = "C:\\Windows\\Temp\\in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
             String line = br.readLine();
 
@@ -22,17 +18,6 @@ public class Program {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
 
     }
