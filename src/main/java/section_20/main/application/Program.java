0b1b3faed3e64af8f7fc6339be30b1256dbdef5c
Ismael Moura <ismaeldesousa2014@outlook.com>
2023-02-13 13:49:03 -0300

    Predicate implementation

--- src/main/java/section_20/main/application/Program.java
+++ src/main/java/section_20/main/application/Program.java
@@ -1,6 +1,7 @@
 package section_20.main.application;
 
 import section_20.main.entities.Product;
+import section_20.main.util.ProductPredicate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,14 +10,16 @@ public class Program {
     public static void main(String[] args) {
         List<Product> list = new ArrayList<>();
 
-        list.add(new Product("TV", 900.00));
-        list.add(new Product("Notebook", 1200.00));
-        list.add(new Product("Tablet", 450.00));
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
 
-        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+        list.removeIf(new ProductPredicate());
 
         for (Product product : list) {
             System.out.println(product);
         }
+
     }
 }
