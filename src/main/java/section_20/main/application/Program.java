85f1896e891a4d6c3e1be09cadab67d1ea06da3f
Ismael Moura <ismaeldesousa2014@outlook.com>
2023-02-17 13:51:23 -0300

    streams pipeline]

--- src/main/java/section_20/main/application/Program.java
+++ src/main/java/section_20/main/application/Program.java
@@ -9,21 +9,15 @@ public class Program {
         List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
 
         Stream<Integer> st1 = list.stream().map(x -> x * 10);
-
         System.out.println(Arrays.toString(st1.toArray()));
 
-        Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
-        System.out.println(Arrays.toString(st2.toArray()));
+        int sum = list.stream().reduce(0, Integer::sum);
+        System.out.println(sum);
 
-        Stream<Integer> st3 = Stream.iterate(0, x -> x + 2).limit(10);
-        System.out.println(Arrays.toString(st3.toArray()));
-
-        Stream<Long> st4 = Stream
-                .iterate(
-                        new Long[] {0L, 1L},
-                        p -> new Long[] {p[1], p[0]+ p[1]}
-                )
-                .map(p -> p[0]);
-        System.out.println(Arrays.toString(st4.limit(50).toArray()));
+        List<Integer> newList = list.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * 10)
+                .toList();
+        System.out.println(Arrays.toString(newList.toArray()));
     }
 }
